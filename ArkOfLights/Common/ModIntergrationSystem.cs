6b35c41293d28592120938596dbefb4853c82109
rafaelkamv <r.hamonangan@yahoo.com>
2022-06-19 12:33:41 +0700

    Addded Common
    Added Common, Boss checklist mod, and The Stars Above mod Integration

---
+++ ArkOfLights/Common/ModIntergrationSystem.cs
@@ -0,0 +1,39 @@
+﻿using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Graphics;
+using System;
+using System.Collections.Generic;
+using Terraria.ModLoader;
+
+namespace ArkOfLights.Common
+{
+    public class ModIntergrationSystem : ModSystem
+    {
+        public override void PostSetupContent()
+        {
+            DoTheStarsAboveIntergration();
+
+            DoBossChecklistIntegration();
+        }
+
+        private void DoTheStarsAboveIntergration()
+        {
+            if (!ModLoader.TryGetMod("TheStarsAbove", out Mod TheStarsAboveMod))
+            {
+                return;
+            }
+        }
+
+        private void DoBossChecklistIntegration()
+        {
+            if (!ModLoader.TryGetMod("BossChecklist", out Mod bossChecklistMod))
+            {
+                return;
+            }
+
+            if (bossChecklistMod.Version < new Version(1, 3, 1))
+            {
+                return;
+            }
+        }
+    }
+}
