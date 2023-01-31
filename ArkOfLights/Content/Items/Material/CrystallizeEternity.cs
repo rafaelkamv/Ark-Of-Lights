fe5448f8c5e0063bd73e6986ccd3917b80c34f21
rafaelkamv <r.hamonangan@yahoo.com>
2023-01-31 08:50:45 +0700

    Added Crystallize Eternity as an item

---
+++ ArkOfLights/Content/Items/Material/CrystallizeEternity.cs
@@ -0,0 +1,35 @@
+using Terraria;
+using Terraria.ID;
+using Terraria.ModLoader;
+using Terraria.Net;
+using Terraria.GameContent.NetModules;
+using Terraria.GameContent.Creative;
+
+namespace ArkOfLights.Content.Items.Material
+{
+    public class CrystallizeEternity : ModItem
+    {
+        public override void SetStaticDefaults()
+        {
+            Tooltip.SetDefault("A material that have seems\nto crystallize for a long time\n[c/04c3f9:Devine Material]");
+            CreativeItemSacrificesCatalog.Instance.SacrificeCountNeededByItemId[Type] = 99;
+        }
+
+        public override void SetDefaults()
+        {
+            Item.width = 32;
+            Item.height = 32;
+            Item.rare = ItemRarityID.Cyan;
+            Item.maxStack = 999;
+            Item.value = Item.sellPrice(platinum: 10);
+        }
+
+        public override void OnResearched(bool fullyResearched)
+        {
+            if (fullyResearched)
+            {
+                CreativeUI.ResearchItem(ModContent.ItemType<GoldenScale>());
+            }
+        }
+    }
+}
