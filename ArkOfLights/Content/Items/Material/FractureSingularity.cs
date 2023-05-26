20bf490eda2ae7a784fffc849892cd68b3013eb2
rafaelkamv <r.hamonangan@yahoo.com>
2023-05-26 15:28:08 +0700

    Added

---
+++ ArkOfLights/Content/Items/Material/FractureSingularity.cs
@@ -0,0 +1,36 @@
+using Terraria;
+using Terraria.ID;
+using Terraria.ModLoader;
+using Terraria.Net;
+using Terraria.GameContent.NetModules;
+using Terraria.GameContent.Creative;
+
+namespace ArkOfLights.Content.Items.Material
+{
+    public class EssenceofClarity : ModItem
+    {
+        public override void SetStaticDefaults()
+        {
+            DisplayName.SetDefault("Fractured Singularity");
+            Tooltip.SetDefault("Main component in the making of Singularities\n[c/04c3f9:Legendary Material]");
+            //Remind me to recolor the rarity
+            CreativeItemSacrificesCatalog.Instance.SacrificeCountNeededByItemId[Type] = 99;
+        }
+
+        public override void SetDefaults()
+        {
+            Item.width = 16;
+            Item.height = 16;
+            Item.rare = ItemRarityID.Purple;
+            Item.maxStack = 1;
+        }
+
+        public override void OnResearched(bool fullyResearched)
+        {
+            if (fullyResearched)
+            {
+                CreativeUI.ResearchItem(ModContent.ItemType<FracturedSingularity>());
+            }
+        }
+    }
+}
